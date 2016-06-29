022a60904ecf35990b89c9ad489bc52b90ff34b1
sam.mentink <samari.b.dj@gmail.com>
2016-06-29 14:24:43 +1200

    next trial code

--- quote.js
+++ quote.js
@@ -1,6 +1,36 @@
-$(function () {
 
+// array //
 
+Var quotes = ["quote 1", "Quote 2" , "Quote 3"]
 
-    }
+// randomness input //
+
+// var randomGenerator = Math.floor(Math.random() * quotes.length);
+
+var displayedQuote;
+var currentIdx;
+
+function randomWholeNum() {
+    var idx = Math.floor(Math.random() * quotes.length)
+    displayedQuote = quotes[idx];
+
+    currentIdx = idx;
+    $('#flex-item').html(quotes[idx].quote);
+
 }
+
+// function possibilities //
+
+$(function(){
+    updateQuote();
+    $('#generator').click(updateQuote);
+})
+
+
+// possible button //
+
+// $(".btn").on("click", function()) {
+ //   getQuote();
+// });
+
+
