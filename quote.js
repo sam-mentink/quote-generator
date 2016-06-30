12bbb267d24d887cf0497abf638efae7bbfd70b6
sam.mentink <samari.b.dj@gmail.com>
2016-06-30 16:01:05 +1200

    final

--- quote.js
+++ quote.js
@@ -1,32 +1,77 @@
 
 // array //
 
-Var quotes = ["quote 1", "Quote 2" , "Quote 3"]
+//Var quotes = ["Quote 1", "Quote 2" , "Quote 3"]
 
 // randomness input //
 
 // var randomGenerator = Math.floor(Math.random() * quotes.length);
 
-var displayedQuote;
-var currentIdx;
+//var displayedQuote;
+//var currentIdx;
 
-function randomWholeNum() {
-    var idx = Math.floor(Math.random() * quotes.length)
-    displayedQuote = quotes[idx];
+//function randomWholeNum() {
+//    var idx = Math.floor(Math.random() * quotes.length)
+//    displayedQuote = quotes[idx];
 
-    currentIdx = idx;
-    $('#flex-item').html(quotes[idx].quote);
+//    currentIdx = idx;
+//    $('#flex-item').html(quotes[idx].quote);
 
-}
+//}
 
 // function possibilities //
 
-$(function(){
-    updateQuote();
-    $('#generator').click(updateQuote);
-})
+//$(function(){
+//    updateQuote();
+//    $('#generator').click(updateQuote);
+//})
 
+//function myFunction() {
+//    document.getElementById("generator").onclick = function() {makerequest('test.html');};
+//}
 
+//(function() {
+ //   var httpRequest;
+ var button = document.getElementById("generator");
+    button.addEventListener("click", makeRequest());
+ var display = document.querySelector(".flex-item");
+//}
+//http://api.icndb.com/jokes/random
+function makeRequest() {
+    $.ajax({
+        url:"http://api.icndb.com/jokes/random",
+        method: "GET",
+        data: 'json',
+        success : function (data) {
+            display.innerHTML = (data.value.joke)
+        }
+
+    })
+
+}
+//     httpRequest = new XMLHttpRequest();
+
+//     if (!httpRequest) {
+//         alert("Death to Chuck Norris");
+//         return false;
+//     }
+//     httpRequest.onreadystatechange = alertContents;
+//     httpRequest.open('GET', 'http://api.icndb.com/jokes/random', false);
+//     httpRequest.send();
+// }
+
+// function alertContents() {
+//     if (httpRequest.ready state === XMLHttpRequest.DONE) {
+//         if (httpRequest.status === 200) {
+//             alert (httpRequest.responseText);
+//         } else {
+//             alert ('there is a problem with Chuck Norris');
+//         }
+//     }
+//}
+
+//})();
+//<button id="generator">quotanator</button>//
 // possible button //
 
 // $(".btn").on("click", function()) {
@@ -34,3 +79,10 @@ $(function(){
 // });
 
 
+//<button id="generator".onclick>make Request
+
+//<span id="generator" style="cursor: pointer; text-decoration: underline"</span>
+
+//<span id="generator".onclick>Hello World</span>
+
+//<button onclick="generator">Click me</button>
